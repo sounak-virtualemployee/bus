7c4976500eb7f188614053713ffdee2def343339
Sounak Saha <sounaksaha@Mac.lan>
2025-07-21 23:30:02 +0530

    ki

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 // app.use('/api/buses', busRoutes);
